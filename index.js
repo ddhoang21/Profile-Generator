7f6d7dd18034b8ec8926bc61f3401e0af09e0cab
DDH <ddhoang1993@gmail.com>
2020-01-26 18:04:07 -0800

    added body in generateHTML file

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub username: ",
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "What is your favorite color?",
+        choices: ["green", "blue", "pink", "red"],
+    }
+];
+
+function writeToFile(fileName, data) {}
+
+function init() {}
+
+init();
